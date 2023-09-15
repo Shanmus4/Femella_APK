8d1646719930e6168ee027efa822651617690a2d
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-09-15 11:36:10 +0530

    Added checkbox screen

---
+++ app/src/main/java/com/ash/ovulio/infocollect/privacy.kt
@@ -0,0 +1,35 @@
+package com.ash.ovulio.infocollect
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import com.ash.ovulio.R
+import com.ash.ovulio.infocollect.privacy
+
+
+class privacy : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_privacy)
+
+        val mButton = findViewById<View>(R.id.privacyScreenButton) as Button
+        val mCheckBox = findViewById<CheckBox>(R.id.privacyCheckBox1) as CheckBox
+        mCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                mButton.isEnabled = true
+                mButton.setOnClickListener{
+                    Toast.makeText(this@privacy, "Hi ", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                mButton.isEnabled = false
+            }
+        }
+    }
+}
--- app/src/main/java/com/ash/ovulio/login/SignIn.kt
+++ app/src/main/java/com/ash/ovulio/login/SignIn.kt
@@ -1,17 +1,15 @@
 package com.ash.ovulio.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.graphics.Color
-import androidx.core.content.ContextCompat
 import com.ash.ovulio.R
+import com.ash.ovulio.infocollect.privacy
 
 class SignIn : ComponentActivity() {
 
@@ -30,16 +28,18 @@ class SignIn : ComponentActivity() {
 //
 
         signInButton.setOnClickListener{
-            if (emailText.text.toString().equals("User", true) && passwordText.text.toString().equals("pass", ignoreCase = true)){
+            if (passwordText.text.toString().equals("pass")){
                 // show next page event
-                errorMessage.visibility = View.VISIBLE
-                errorMessage.text = "Correct!!"
-            }
+                val buttonClick = findViewById<Button>(R.id.SignInButton)
+                buttonClick.setOnClickListener {
+                    Toast.makeText(this@SignIn, "Hi " + emailText.text.toString(), Toast.LENGTH_LONG).show();
+                    val intent = Intent(this, privacy::class.java)
+                    startActivity(intent)
+            }}
             else{
                 // show error message
-
                 errorMessage.visibility = View.VISIBLE
-                errorMessage.text = "Incorrect Password"
+                errorMessage.text = getString(R.string.password_error)
 
             }
         }
